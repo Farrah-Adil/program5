a2cdc49cf5a159a06a1064a3219ff853f00bd042
Farah Adil <fadil@cedrus.com>
2023-07-21 15:45:32 +0500

    Commit Feat: Program5 Enter some extra method

--- Program5/Program.cs
+++ Program5/Program.cs
@@ -6,23 +6,23 @@ class Exponent
     {
         int b, e;
         Console.Write("Enter base:");
-        int.TryParse(Console.ReadLine(), out b);
+        int.TryParse(Console.ReadLine(),out b);
         Console.Write("Enter exponent:");
         int.TryParse(Console.ReadLine(), out e);
         Console.WriteLine("{0} to the power {1} = {2}", b, e, IntegerPower(b, e));
-
+       
     }
 
     static int IntegerPower(int b, int e)
     {
         if (e < 1) throw new ArgumentException("Exponent must be positive");
         int power = 1;
-        int count = 1;
+        int num = 1;
 
-        while (count <= e)
+        while (num <= e)
         {
             power *= b;
-            count++;
+            num++;
         }
 
         return power;
