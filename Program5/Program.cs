ecf02159f49dbd5a4f5073aac2277c4ca1d80017
Farah Adil <fadil@cedrus.com>
2023-07-21 15:38:24 +0500

    Commit Feat: Program5 Done

---
+++ Program5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Exponent
+{
+    static void Main()
+    {
+        int b, e;
+        Console.Write("Enter base:");
+        int.TryParse(Console.ReadLine(), out b);
+        Console.Write("Enter exponent:");
+        int.TryParse(Console.ReadLine(), out e);
+        Console.WriteLine("{0} to the power {1} = {2}", b, e, IntegerPower(b, e));
+
+    }
+
+    static int IntegerPower(int b, int e)
+    {
+        if (e < 1) throw new ArgumentException("Exponent must be positive");
+        int power = 1;
+        int count = 1;
+
+        while (count <= e)
+        {
+            power *= b;
+            count++;
+        }
+
+        return power;
+    }
+}
